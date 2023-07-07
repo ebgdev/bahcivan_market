a0bb7ef36699afabc25c5fead3bee5e3ef77da0a
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-07-08 02:43:26 +0300

    8.3-4:43 done.

--- store/views.py
+++ store/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
-# Create your views here.
-
-
+from carts.views import _cart_id
+from django.http import HttpResponse
+from carts.models import CartItem
 def store(request,category_slug=None):
     categories = None
     products = None
@@ -25,6 +25,9 @@ def store(request,category_slug=None):
 def product_detail(request,category_slug,product_slug):
     try:
         single_product = Product.objects.get(category__slug= category_slug,slug = product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id= _cart_id(request),product=single_product).exists()
+        return HttpResponse(in_cart)
+        exit()
     except Exception as e:
         raise e
     context = {
