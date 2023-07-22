9cc0b06186da5044901e6df97533278835b6b935
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-07-22 22:15:43 +0300

    8.4 cart icon and functions get done.

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect,get_object_or_404
+# from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 from .models import Cart,CartItem 
 # from django.http import HttpResponse 
