2e4291118bdd8eb49a81f08103635a995f862b9a
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-09-07 21:49:28 +0300

    Till 10.5:24 just before migrations

--- carts/views.py
+++ carts/views.py
@@ -1,23 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect,get_object_or_404
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product,Variation
 from .models import Cart,CartItem 
 # from django.http import HttpResponse 
 
 def _cart_id(request):
     cart = request.session.session_key
     if not cart:
-        cart = request.session.create()
+        cart = request.session.create() 
     return cart
+
+
 # to add product to cart one by one
 def add_cart(request,product_id):
+    product = Product.objects.get(id=product_id)
+    product_variation = []
     if request.method == "POST":
-        weight = request.POST['weight']
-        volume = request.POST['volume']
-        print(weight,volume)
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            try:
+                variation = Variation.objects.get(product = product,variation_category__iexact=key,variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                pass
     #get the product
-    product = Product.objects.get(id=product_id) 
+     
     try:
         #get the cart using cart_id show up in the session
         cart = Cart.objects.get(cart_id = _cart_id(request)) 
