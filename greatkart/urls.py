241070c378711e0e9e466e6c6b7f458d8f5e0e2c
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-09-17 20:30:28 +0300

    12.1 register and form get created.

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', views.home, name ='home'),
     path('cart/',include('carts.urls')),
     path('store/',include('store.urls')),
+    path('accounts/',include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
