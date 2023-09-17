af8d86c54a67bb9af58eb0130fcabb447c7f7f30
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-09-17 13:18:06 +0300

    11.3 is done.

--- carts/views.py
+++ carts/views.py
@@ -82,23 +82,26 @@ def add_cart(request,product_id):
 
 
 # to remove product added to cart one by one
-def remove_cart(request,product_id):
+def remove_cart(request,product_id,cart_item_id):
     cart = Cart.objects.get(cart_id = _cart_id(request))
     product = get_object_or_404(Product, id
     =product_id)
-    cart_item = CartItem.objects.get(product=product ,cart=cart)
-    if cart_item.quantity > 1:
-        cart_item.quantity -=1
-        cart_item.save()
-    else:
-        cart_item.delete()
+    try:
+        cart_item = CartItem.objects.get(product=product ,cart=cart,id=cart_item_id)
+        if cart_item.quantity > 1:
+            cart_item.quantity -=1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except:
+        pass            
     return redirect('cart')
 
 # to remove whole product regardless of the amount
-def remove_cart_item(request,product_id):
+def remove_cart_item(request,product_id,cart_item_id):
     cart = Cart.objects.get(cart_id = _cart_id(request))
-    product = get_object_or_404(Product,id = product_id)
-    cart_item = CartItem.objects.get(product=product,cart=cart)
+    product = get_object_or_404(Product,id = product_id,)
+    cart_item = CartItem.objects.get(product=product,cart=cart,id=cart_item_id)
     cart_item.delete()
     return redirect('cart')
         
