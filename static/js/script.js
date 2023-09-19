ac902bed85759b73206ee8f57008dd547051f9db
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-09-19 17:19:23 +0300

    12.4  fadeout didn't work, FIX

--- static/js/script.js
+++ static/js/script.js
@@ -58,3 +58,13 @@ $(document).ready(function() {
 }); 
 // jquery end
 
+
+setTimeout(function(){
+    $('#message').fadeOut('slow')
+},4000)
+
+// $(document).ready(function() {
+//     setTimeout(function() {
+//         $('#message').fadeOut('slow');
+//     }, 4000);
+// });
