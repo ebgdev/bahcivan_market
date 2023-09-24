2b768c81ffc5ab4016763a506eb01dd65ea7486f
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-09-24 14:22:41 +0300

    12.5 Logout get done.

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render , redirect
+from django.contrib import messages ,auth
+from django.contrib.auth.decorators import login_required
 from .forms import RegistrationForm
 from .models import Account
-from django.contrib import messages
+
+
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -27,7 +30,25 @@ def register(request):
     return render(request,'accounts/register.html',context)
 
 def login(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            # messages.success(request,'Giriş Yapıldı')
+            return redirect('home')
+        else:
+            messages.error(request,'Geçersiz giriş')
+            return redirect('login')
+    
     return render(request,'accounts/login.html')
 
+
+@login_required(login_url = 'login')
 def logout(request):
-    return render(request,'accounts/logout.html')
+    auth.logout(request)
+    messages.success(request,'Çıkış yapıldı')
+    return redirect('login')
