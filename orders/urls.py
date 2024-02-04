7bb4c48bc3c67ccddfd111b5e31730dea2f63cff
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2024-02-04 19:19:01 +0300

    16.5 is done.

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views 
+# from .models import Payment
 
 urlpatterns = [
     path('place_order/', views.place_order, name ='place_order'),
+    path('payments/',views.payments,name='payments'),
     ]
