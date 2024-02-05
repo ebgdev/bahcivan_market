1d0a603832bda7a9c2a9c944744e7b4d32c5a847
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2024-02-05 21:50:27 +0300

    16.6 get completed.

--- orders/views.py
+++ orders/views.py
@@ -58,15 +58,14 @@ def place_order(request, total=0, quantity=0):
             data.order_number = order_number
             data.save()
 
-            # order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
-            # context = {
-            #     'order': order,
-            #     'cart_items': cart_items,
-            #     'total': total,
-            #     'tax': tax,
-            #     'grand_total': grand_total,
-            # }
-             # return render(request, 'orders/payments.html', context)
-            return redirect('checkout')
+            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+            context = {
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                # 'tax': tax,
+                'grand_total': grand_total,
+            }
+            return render(request, 'orders/payments.html', context)
     else:
         return redirect('checkout')
